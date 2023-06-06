a14a8fa8710c58e1ed74407363e8e0a6cf54a36e
OMGboom7 <novaboom1029@gmail.com>
2023-06-06 22:33:15 +0800

    feat:

---
+++ demo08/moz-todo-react/src/App.js
@@ -0,0 +1,35 @@
+import Todo         from './component/Todo';
+import FilterButton from './component/FilterButton'
+import Form         from './component/Form'
+
+function App (props) {
+	const taskList = props.tasks?.map((task) => (
+			<Todo
+					id={ task.id }
+					name={ task.name }
+					completed={ task.completed }
+					key={ task.id }
+			/>
+	));
+	return (<div className="todoapp stack-large">
+		<h1>TodoMatic</h1>
+		<Form/>
+		<div className="filters btn-group stack-exception">
+			<FilterButton />
+			<FilterButton />
+			<FilterButton />
+		</div>
+		<h2 id="list-heading">
+			3 tasks remaining
+		</h2>
+		<ul
+				role="list"
+				className="todo-list stack-large stack-exception"
+				aria-labelledby="list-heading"
+		>
+			{ taskList }
+		</ul>
+	</div>);
+}
+
+export default App;
