fbbf771688996def2e18d9628f5ffead243779d9
OMGboom7 <novaboom1029@gmail.com>
2023-06-07 00:25:04 +0800

    feat:

---
+++ demo09/ToolBox/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './components/NavBar.jsx'
+
+function App () {
+
+	return (<div>
+		<Navbar/>
+		<div>
+			<p className='text-3xl font-bold underline'>123123</p>
+			<p>garegrea</p>
+			<p>jh64qwj</p>
+
+		</div>
+	</div>)
+}
+
+export default App
