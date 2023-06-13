ee34beb0ce2dae0c8905d6b8258b4549eb3b22ae
OMGboom7 <novaboom1029@gmail.com>
2023-06-13 15:17:24 +0800

    Update main.js and index.html to include user-customizable inputs and measurement conversions.

--- demo09/ToolBox-js/src/App.jsx
+++ demo09/ToolBox-js/src/App.jsx
@@ -1,16 +1,16 @@
-import Navbar from './components/NavBar.jsx'
+import Navbar from "./components/NavBar.jsx";
 
-function App () {
-
-	return (<div>
-		<Navbar/>
-		<div>
-			<p className='text-3xl font-bold underline'>123123</p>
-			<p>garegrea</p>
-			<p>jh64qwj</p>
-
-		</div>
-	</div>)
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <p className="text-3xl font-bold underline">123123</p>
+        <p className="bg-blue text-brown-300">garegrea</p>
+        <p>jh64qwj</p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- demo09/ToolBox-js/src/components/NavBar.jsx
+++ demo09/ToolBox-js/src/components/NavBar.jsx
@@ -1,40 +1,13 @@
-import React from 'react';
-// import 'tailwindcss/tailwind.css'
-
-function Navbar () {
-	return (<nav className="bg-green">
-			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-				<div className="flex items-center justify-between h-16">
-					<div className="flex items-center">
-						<div className="flex-shrink-0">
-							<span className="text-white">Logo</span>
-						</div>
-						<div className="hidden md:block">
-							<div className="ml-10 flex items-baseline space-x-4">
-								<a
-									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-								>
-									Home
-								</a>
-								<a
-									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-								>
-									About
-								</a>
-								<a
-									href="#"
-									className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-								>
-									Contact
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</nav>);
+function Navbar() {
+  return (
+    <nav className="bg-green">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-12 table">
+        <div className="table-cell align-middle">
+          <p className="text-white">ToolBox</p>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
